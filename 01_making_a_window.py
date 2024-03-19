195152964bb6de86d853c5435b3aaf4d6e171650
hannankhan888 <hannankhan888@gmail.com>
2024-03-18 22:37:08 -0600

    made a more simplified app for hello world

--- 01_making_a_window.py
+++ 01_making_a_window.py
@@ -1,16 +1,12 @@
 import sys
 
-# from PyQt5.QtCore import
 from PyQt5.QtWidgets import QApplication, QLabel, QWidget
 
 
 def main():
     app = QApplication(sys.argv)
     w = QWidget()
-    label = QLabel(w)
-    label.setText("Hello World")
     w.setGeometry(100, 100, 1600, 900)
-    label.move(800, 450)
     w.setWindowTitle("PyQt6 - Hello World Example")
     w.show()
     sys.exit(app.exec())
@@ -18,4 +14,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
