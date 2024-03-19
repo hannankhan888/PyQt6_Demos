efc61574cfaeceb321d1bb38767c37da43b6cc55
hannankhan888 <hannankhan888@gmail.com>
2024-03-18 22:38:09 -0600

    made it even more simpler for starter example

--- 01_making_a_window.py
+++ 01_making_a_window.py
@@ -1,13 +1,11 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication, QLabel, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget
 
 
 def main():
     app = QApplication(sys.argv)
     w = QWidget()
-    w.setGeometry(100, 100, 1600, 900)
-    w.setWindowTitle("PyQt6 - Hello World Example")
     w.show()
     sys.exit(app.exec())
 
